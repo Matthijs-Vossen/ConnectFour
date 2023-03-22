b45315d6a1720d4764b502b66c5f5d54ce7727fe
MatthijsDSAI <matthijsvossen@live.nl>
2023-03-22 11:28:59 +0100

    Added Clippy & OS check

--- Graphics.py
+++ Graphics.py
@@ -1,5 +1,7 @@
 import tkinter as tk
-from tkinter import Button
+from tkmacosx import Button
+from PIL import ImageTk, Image
+import platform
 from MiniMax import *
 from Logic import *
 
@@ -12,13 +14,15 @@ class Graphics:
 
         # Create the game board frame and buttons
         self.game_frame = tk.Frame(self.root)
-        self.game_frame.pack(side="top")
+        self.game_frame.pack(side="left")
         self.start_game()
 
-        # Create the main menu frame and buttons
-        self.menu_frame = tk.Frame(self.root)
-        self.menu_frame.pack(side="bottom")
-        self.create_main_menu()
+        # Create the AI menu
+        self.ai_frame = tk.Frame(self.root)
+        self.ai_frame.pack(side="right")
+        self.ai_frame.config(width=300, height=150)
+        self.ai_frame.pack_propagate(0)
+        self.create_ai_frame()
 
         self.root.mainloop()
 
@@ -28,13 +32,21 @@ class Graphics:
             self.turn_label.destroy()
         except:
             pass
+
         w,h = 5,3
+        os_windows = True
+        if platform.system() == 'Darwin':
+            os_windows = False
         # Create the game board as a grid of buttons
         self.buttons = []
         for i in range(6):
             row = []
             for j in range(7):
-                button = Button(self.game_frame, width=w, height=h, bg='white',
+                if os_windows:
+                    button = tk.Button(self.game_frame, width=w, height=h, bg='white',
+                                    command=lambda column=j: self.make_move(column))
+                else:
+                    button = Button(self.game_frame, width=50, height=50, bg='white',
                                 command=lambda column=j: self.make_move(column))
                 button.grid(row=i, column=j, padx=2, pady=2)
                 row.append(button)
@@ -43,6 +55,13 @@ class Graphics:
         # Create a label to display the current player's turn
         self.turn_label = tk.Label(self.game_frame, text=f"Player {self.game.player}'s turn", font=("Arial", 16))
         self.turn_label.grid(row=6, columnspan=7)
+
+        # Create the menu buttons
+        start_game_button = tk.Button(self.game_frame, text="New Game", command=self.start_game)
+        start_game_button.grid(row=7, columnspan=7)
+
+        settings_button = tk.Button(self.game_frame, text="Settings", command=self.settings)
+        settings_button.grid(row=8, columnspan=7)
 
     def make_move(self, column):
         mm = MiniMax()
@@ -70,13 +89,19 @@ class Graphics:
             self.turn_label.config(text=f"Player {self.game.player}'s turn")
         print(mm.get_best_move(self.game,3))
 
-    def create_main_menu(self):
-        # Create the menu buttons
-        start_game_button = tk.Button(self.menu_frame, text="New Game", command=self.start_game)
-        start_game_button.pack(side="left", padx=10)
+    def create_ai_frame(self):
+        # Create an object of tkinter ImageTk
+        self.img = ImageTk.PhotoImage(Image.open("images/clippy.png").resize((100,100), Image.ANTIALIAS))
 
-        settings_button = tk.Button(self.menu_frame, text="Settings", command=self.settings)
-        settings_button.pack(side="left", padx=10)
+        # Create a label to display the image
+        self.img_label = tk.Label(self.ai_frame, image=self.img)
+        self.img_label.pack(side="bottom")
+
+        self.clippy_text_label = tk.Label(self.ai_frame, text="Lorem ipsum.", height=100, wraplength=290)
+        self.clippy_text_label.pack(side="top")
+
+    def set_suggestion(self, text: str):
+        self.clippy_text_label.config(text=text)
 
     def settings(self):
         pass
