e65f4c7a77461b1194449a72d29c42d8a25024c3
Willem-P1 <w.ploegstra@student.maastrichtuniversity.nl>
2023-03-24 17:38:20 +0100

    Minimax comments + kleine performance boost

--- MiniMax.py
+++ MiniMax.py
@@ -15,8 +15,12 @@ class MiniMax:
         board.unmake_move(col)
         
     def get_best_move(self, logic : Logic, depth):
+        """
+        Function to find the best move using minimax with alpha beta pruning
+        It will check for each possible move what minimax value the game will have
+        returns the move with the greatest/lowest minimax value based on which player has to play
+        """
         board = copy.deepcopy(logic)
-        
         if board.player == 1:
             best_i = -1
             max_v = -math.inf
@@ -25,7 +29,6 @@ class MiniMax:
                     continue
                 value = self.minimax(board,depth,-math.inf,math.inf,2)
                 self.unmake_move(board,i)
-
                 if value > max_v:
                     max_v = value
                     best_i = i
@@ -38,7 +41,6 @@ class MiniMax:
                     continue
                 value = self.minimax(board,depth,-math.inf,math.inf,1)
                 self.unmake_move(board,i)
-
                 if value < min_v:
                     min_v = value
                     best_i = i
@@ -46,6 +48,11 @@ class MiniMax:
 
     
     def minimax(self, board, depth,a,b, player):
+        """
+        Recursive minimax function with alpha beta pruning
+        Evaluates each move by playing each possible move and extracts the best possible strategy by evaluating the possible states the move will lead to
+        The function will just evaluate the value of a given state of the board
+        """
         score = board.check_win()
         if depth == 0 and score == None:
             return self.evaluate_board(board)
@@ -86,8 +93,12 @@ class MiniMax:
     
     def evaluate_board(self, b):
         """
-        Evaluates the given board for the given player.
-        Returns a score representing the strength of the player's position.
+        Evaluates the given board.
+        Returns a score representing the strength of the position.
+        A positive score means 'red' is winning, a negative score means 'yellow' is winning.
+        It calculates the score by evaluating all the possible squares in lines of 4 on the board
+        All these evaluations are summed together to get the final score
+        See evaluate_window() to see how the 4 squares are evaluated
         """
         score = 0
         # Check rows for wins
@@ -120,24 +131,28 @@ class MiniMax:
     def evaluate_window(self, window):
         """
         Evaluates a window of four pieces for the given player.
-        Returns a score representing the strength of the player's position in the window.
+        Returns a score representing the strength of the position in the window.
+        A positive score means 'red' is winning, a negative score means 'yellow' is winning.
+        It gives a value of +/- 10 if a player has 3 stones in a row
+        It gives a value of +/- 5 if a player has 3 stones in a row
+        It gives a value of +/- 2 if a player has 3 stones in a row
         """
-        plus = 'red'
-        neg = 'yellow'
-        score = 0
+        plus = window.count('red')
+        neg = window.count('yellow')
+        no = window.count(' ')
         
-        if window.count(plus) == 3 and window.count(' ') == 1:
-            score += 10
-        elif window.count(plus) == 2 and window.count(' ') == 2:
-            score += 5
-        elif window.count(plus) == 1 and window.count(' ') == 3:
-            score += 2
+        if plus == 3 and no == 1:
+            return 10
+        elif plus == 2 and no == 2:
+            return 5
+        elif plus == 1 and no == 3:
+            return 2
         
-        if window.count(neg) == 3 and window.count(' ') == 1:
-            score -= 10
-        elif window.count(neg) == 2 and window.count(' ') == 2:
-            score -= 5
-        elif window.count(neg) == 1 and window.count(' ') == 3:
-            score -= 2
+        if neg == 3 and no == 1:
+            return -10
+        elif neg == 2 and no == 2:
+            return -5
+        elif neg == 1 and no == 3:
+            return -2
         
-        return score
+        return 0
